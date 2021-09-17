9e3366befd2e1975a3b86ab7e8c019e6a3e8c65d
kroos783 <hjhhj@hotmail.com>
2021-09-17 17:05:15 +0200

    add talisman helpers

--- helpers.py
+++ helpers.py
@@ -18,6 +18,6 @@ def apology(message, code=400):
                          ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
             s = s.replace(old, new)
         return s
-    return render_template("apology.html", top=code, csp=csp, bottom=escape(message)), code
+    return render_template("apology.html", top=code, bottom=escape(message)), code
 
 
