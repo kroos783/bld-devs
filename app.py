b8d69cd7fea8c8edaabb66a171838beb2b890079
kroos783 <hjhhj@hotmail.com>
2021-09-21 01:51:45 +0200

    add 2eme mail

--- app.py
+++ app.py
@@ -47,9 +47,8 @@ app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'bld.devs@gmail.com'
 app.config['MAIL_PASSWORD'] = 'Auto78310'
-app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_DEFAULT_SENDER'] = 'bld.devs@gmail.com'
+app.config['MAIL_DEBUG'] = True
 
 app.config['RECAPTCHA_USE_SSL']= False
 app.config['RECAPTCHA_PUBLIC_KEY']='6LeIwmgcAAAAAIqzcLYo9AjypZn8nCJ2sUKzqaDz'
